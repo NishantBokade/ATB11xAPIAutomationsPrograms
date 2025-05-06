34e52caa5528a64f48c7113fa4aa14ffee54585a
nisha <nishantbokade@gmail.com>
2025-05-07 00:28:01 +0530

    Push code-7th april

---
+++ src/test/java/com/thetestingacademy/ex_04_RestAssured_HTTP_Methods/GET/APITesting_Lab07_GET_BDDStyle.java
@@ -0,0 +1,23 @@
+package com.thetestingacademy.ex_04_RestAssured_HTTP_Methods.GET;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+public class APITesting_Lab07_GET_BDDStyle {
+
+    @Test
+    public void test_GET_POSITIVE(){
+        String pin_code = "388620";
+        RestAssured
+                .given()
+                .baseUri("https://api.zippopotam.us")
+                .basePath("/IN/" + pin_code)
+                .when()
+                .log()
+                .all()
+                .get()
+                .then()
+                .log().all()
+                .statusCode(200);
+    }
+}
